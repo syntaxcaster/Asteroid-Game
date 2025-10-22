b7e43b1b3966a079ebc4c2b2206b44d0a71c455e
syntaxcaster <k29.1997@gmail.com>
2025-10-22 12:16:01 -0700

    v0.02 - added player and circle shape class, added 'q' to quit, draw player triangle to screen, added PLAYER_RADIUS constant to constants.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
 
@@ -9,6 +10,15 @@ def main():
     # Initialize our display window
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    # Set our game speed using an FPS counter
+    fps = pygame.time.Clock()
+
+    # Delta time counter
+    dt = 0
+
+    # Instantiate our Player object
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     # Game loop
     while True:
         
@@ -16,19 +26,20 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    return
 
         # Fill the background of the screen black
         screen.fill("black")
 
-        # Flip the screen, dumping old content and displaying new content
-        pygame.display.flip
+        # Draw our player to the screen
+        player.draw(screen)
 
+        # Flip the screen, dumping old content and displaying new content
+        pygame.display.flip()
+
+        dt = fps.tick(60) / 1_000
 
 if __name__ == "__main__":
     main()
-
-"""
-Starting Asteroids!
-Screen width: 1280
-Screen height: 720
-"""
