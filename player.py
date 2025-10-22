54d63b5905eeb08ac8e929ca22149d104c241972
syntaxcaster <k29.1997@gmail.com>
2025-10-22 12:37:13 -0700

    v0.04 - added forward/backward movement to player and PLAYER_SPEED to constants

--- player.py
+++ player.py
@@ -1,6 +1,8 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
+from constants import (PLAYER_RADIUS,
+                      PLAYER_TURN_SPEED,
+                      PLAYER_SPEED)
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -17,6 +19,10 @@ class Player(CircleShape):
     
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
     
     def update(self, dt):
         keys = pygame.key.get_pressed()
@@ -25,6 +31,10 @@ class Player(CircleShape):
             self.rotate(dt*-1)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(dt*-1)
 
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
